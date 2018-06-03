6dd7b31c028896ca18065962af0dfa386b041cb5
Hannah Hoshino <hannah@Hannahs-MacBook-Pro.local>
2018-06-03 17:54:02 +0900

    Remove unused file

--- install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.git ~/.git
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-
